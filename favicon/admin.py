91b37d5a12799284f553723ef9936e95283b2111
Yannik Ammann <ammann@arteria.ch>
2014-12-15 09:45:34 +0100

    add admin view for testing

--- favicon/admin.py
+++ favicon/admin.py
@@ -6,3 +6,19 @@ class FaviconAdmin(admin.ModelAdmin):
     list_display = ('title', 'isFavicon')
 
 admin.site.register(Favicon, FaviconAdmin)
+
+
+class FaviconImgAdmin(admin.ModelAdmin):
+    list_display = ('faviconFK', 'rel', 'size', 'faviconImage')
+
+    def queryset(self, request):
+        qs = super(FaviconImgAdmin, self).queryset(request)
+        isFavicon = Favicon.objects.filter(isFavicon = True)
+        if not len(isFavicon) == 1:
+            for n in Favicon.objects.all():
+                n.isFavicon = False
+                return qs
+        isFavicon = isFavicon[0]
+        return qs.filter(faviconFK = isFavicon)
+
+admin.site.register(FaviconImg, FaviconImgAdmin)
