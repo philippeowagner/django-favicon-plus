783a0b9c7e684f4f9ac2e3fc2879af0b3b3053bb
Yannik Ammann <ammann@arteria.ch>
2015-01-21 14:49:09 +0100

    del urls & views

--- favicon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('favicon.views',
-                       url(r'^$',
-                           view='favicon',
-                           name='favicon',
-                           ),
-                       )
